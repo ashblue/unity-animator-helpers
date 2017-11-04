54545a9e7d0e8229d610352d80c54d209b41178e
Ash Blue <ash@blueashes.com>
2017-11-03 23:30:23 -0600

    Added variable base classes

---
+++ Assets/AdncAnimatorVariableStates/Scripts/Variables/VarBool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Adnc.AnimatorVariables.Variables {
+    [System.Serializable]
+    public class VarBool : VarBase<bool> {
+        [Tooltip(VALUE_TOOLTIP)]
+        public bool value;
+
+        public override void SetValue (Animator animator) {
+            animator.SetBool(name, value);
+        }
+
+        public override bool GetValue (Animator animator) {
+            return animator.GetBool(name);
+        }
+    }
+}
---
+++ Assets/AdncAnimatorVariableStates/Scripts/Variables/VarFloat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Adnc.AnimatorVariables.Variables {
+    [System.Serializable]
+    public class VarFloat : VarBase<float> {
+        [Tooltip(VALUE_TOOLTIP)]
+        public float value;
+
+        public override void SetValue (Animator animator) {
+            animator.SetFloat(name, value);
+        }
+
+        public override float GetValue (Animator animator) {
+            return animator.GetFloat(name);
+        }
+    }
+}
