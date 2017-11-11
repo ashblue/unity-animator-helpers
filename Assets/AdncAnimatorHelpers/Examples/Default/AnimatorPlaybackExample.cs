0de26fe4fb1f2d8a130bb95dbb19de2c9e056432
Ash Blue <ash@blueashes.com>
2017-11-11 02:15:33 -0700

    Updated README.md and examples

--- Assets/AdncAnimatorHelpers/Examples/Default/AnimatorPlaybackExample.cs
+++ Assets/AdncAnimatorHelpers/Examples/Default/AnimatorPlaybackExample.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 namespace Adnc.AnimatorHelpers.Examples {
     public class AnimatorPlaybackExample : MonoBehaviour {
+        private Animator anim;
+
         [SerializeField]
         private AnimatorPlayback _completeEvent;
 
@@ -11,11 +13,16 @@ namespace Adnc.AnimatorHelpers.Examples {
         private string _completeMessage = "Detected animation playback as complete";
 
         private void Start () {
+            anim = GetComponent<Animator>();
+
+            // To play with a coroutine that waits for the conditions
             StartCoroutine(DetectEventComplete());
+
+            // To play without any coroutine or wait conditions
+//            _completeEvent.Play(anim);
         }
 
         IEnumerator DetectEventComplete () {
-            var anim = GetComponent<Animator>();
             yield return _completeEvent.PlayCoroutine(anim);
             Debug.Log(_completeMessage);
         }
