3841cb4a59327015f5c38e5582fd319b3cc6a2fc
Ash Blue <ash@blueashes.com>
2017-12-28 23:16:14 -0700

    Removed deprecated Singleton code

--- Assets/AdncAnimatorHelpers/Scripts/AnimatorHelperRuntime/AnimatorHelperRuntime.cs
+++ Assets/AdncAnimatorHelpers/Scripts/AnimatorHelperRuntime/AnimatorHelperRuntime.cs
@@ -1,8 +1,9 @@
 ﻿using Adnc.AnimatorHelpers.HasParameters;
 using UnityEngine;
+using Adnc.Utility.Singletons;
 
 namespace Adnc.AnimatorHelpers {
-    public class AnimatorHelperRuntime : Singleton<AnimatorHelperRuntime> {
+    public class AnimatorHelperRuntime : SingletonBase<AnimatorHelperRuntime> {
         public AnimatorParametersCollection parameters = new AnimatorParametersCollection();
 
         public void Cache (Animator animator) {
